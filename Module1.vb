e243a149a63a4447318afa4f4a4b5a2e80593486
ian russ <ianruss.bernabe@wvcst.edu.ph>
2015-08-01 10:04:24 +0800

    Almost Done...

--- Module1.vb
+++ Module1.vb
@@ -9,15 +9,41 @@
         End If
     End Function
 
+    Function FibSequence()
+
+    End Function
+
     Sub Main()
 
         Dim intNumber As Integer = 0
+        Dim choice As String = ""
 
-        Console.WriteLine("Enter A Number...")
-        intNumber = CInt(Console.ReadLine())
 
-        Console.WriteLine(" Ang ika " & intNumber & " sa fibonacci is = " & Fib(intNumber))
-        Console.ReadLine()
+        While choice.ToUpper <> "Q"
+            Console.WriteLine("A. Display nth term of a fibonacci only")
+            Console.WriteLine("B. Display fibonacci Sequence")
+            Console.WriteLine("Q. Quit")
+            Console.WriteLine("")
+            choice = Console.ReadLine()
+
+            If choice = "A" Then
+                Console.WriteLine("Enter A Number...")
+                intNumber = CInt(Console.ReadLine())
+                Console.WriteLine(" Ang ika " & intNumber & " sa fibonacci is = " & Fib(intNumber))
+            ElseIf choice = "B" Then
+                Console.WriteLine("Enter A Number...")
+                intNumber = CInt(Console.ReadLine())
+                Console.WriteLine(" Ang ika " & intNumber & " sa fibonacci is = " & Fib(intNumber))
+            End If
+        End While
+
+
+
+        
+
+
+        '        Console.ReadLine()
+
 
 
     End Sub
