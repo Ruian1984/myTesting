95691c936517a846cc875cd00587905a57dbf75e
ian russ <ianruss.bernabe@wvcst.edu.ph>
2015-08-01 09:19:28 +0800

    First Few Lines
    First Few Lines only try to add later

---
+++ Module1.vb
@@ -0,0 +1,15 @@
+﻿Module Module1
+
+
+
+    Sub Main()
+
+        Dim intNumber As Integer = 0
+
+        Console.WriteLine("Enter A Number...")
+        intNumber = CInt(Console.ReadLine())
+
+
+    End Sub
+
+End Module
