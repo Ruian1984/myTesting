d85a7a1015b56ff39a9edd023f2b0dd5f7ddc9a9
ian russ <ianruss.bernabe@wvcst.edu.ph>
2015-08-01 09:56:36 +0800

    May Gin dugang nga lines
    OK na ni pro matyagan ko may kulang...

--- Module1.vb
+++ Module1.vb
@@ -2,8 +2,10 @@
 
 
     Function Fib(ByVal intNumber As Integer) As Integer
-        If intNumber = 0 Then
+        If intNumber <= 2 Then
             Return 1
+        Else
+            Return Fib(intNumber - 2) + Fib(intNumber - 1)
         End If
     End Function
 
@@ -13,6 +15,9 @@
 
         Console.WriteLine("Enter A Number...")
         intNumber = CInt(Console.ReadLine())
+
+        Console.WriteLine(" Ang ika " & intNumber & " sa fibonacci is = " & Fib(intNumber))
+        Console.ReadLine()
 
 
     End Sub
